ca9f1a13d5697410d66e6a09b9417dcca9b0e245
Riaz Uddin Masum <masum@activelava.net>
2022-10-29 02:09:03 +0600

    demo

--- pages/Login.vue
+++ pages/Login.vue
@@ -1,3 +1,4 @@
 <template>
   <h3 class="bg-green-600 text-white p-4">Hi Login</h3>
+  <NuxtLink to="/" class="bg-red-600 text-white p-3 mt-2 ml-2">Home</NuxtLink>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,21 @@
 <template>
   <h3 class="text-white bg-pink-600 p-5">Hi there</h3>
-  <button @click="runMe">Run Now</button>
-  <div>{{ resutls }}</div>
+  <NuxtLink to="login" class="bg-red-600 text-white p-3 mt-2 ml-2">Login</NuxtLink>
+  <button @click="runMe" class="bg-red-600 text-white p-3 mt-2 ml-2">Run Now</button>
+  <div>{{ results?.title }}</div>
 </template>
 <script>
 export default {
   data(){
     return {
-      results: null;
+      results: null,
     }
-  }
+  },
   methods:{
     runMe(){
     fetch('https://jsonplaceholder.typicode.com/todos/1')
   .then(response => response.json())
-  .then(json => this.resulrs = json)
+  .then(json => this.results = json)
   }
   }
 }
