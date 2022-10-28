244ff2673810129e2f41d76c072ad4c9b7ca191b
Riaz Uddin Masum <masum@activelava.net>
2022-10-29 02:14:11 +0600

    link change

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <h3 class="text-white bg-pink-600 p-5">Hi there</h3>
-  <NuxtLink to="login" class="bg-red-600 text-white p-3 mt-2 ml-2">Login</NuxtLink>
+  <NuxtLink to="/login" class="bg-red-600 text-white p-3 mt-2 ml-2">Login</NuxtLink>
   <button @click="runMe" class="bg-red-600 text-white p-3 mt-2 ml-2">Run Now</button>
   <div>{{ results?.title }}</div>
 </template>
@@ -9,7 +9,7 @@ export default {
   data(){
     return {
       results: null,
-    }
+    } 
   },
   methods:{
     runMe(){
