3450322b55e430671d8bfd6d21a4dbf40e101233
masumonline <masum@activelava.net>
2022-10-29 01:25:06 +0600

    1 file modified

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,21 @@
 <template>
   <h3 class="text-white bg-pink-600 p-5">Hi there</h3>
+  <button @click="runMe">Run</button>
+  <div>{{ resutls }}</div>
 </template>
+<script>
+export default {
+  data(){
+    return {
+      results: null;
+    }
+  }
+  methods:{
+    runMe(){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => this.resulrs = json)
+  }
+  }
+}
+</script>
